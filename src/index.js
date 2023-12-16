a0677cd4e70aa529309a83fcdb447a400377627c
Colin Hermack <chermack@purdue.edu>
2023-12-16 15:13:30 -0500

    Cleaned up code and added loading screen

--- src/index.js
+++ src/index.js
@@ -11,45 +11,52 @@ class WeatherApp extends React.Component {
   }
 
   componentDidMount() {
+    let weatherJSON = {}
+
     //Get GPS coordinates
     let latitude = 0;
     let longitude = 0;
-    navigator.geolocation.getCurrentPosition(position => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
 
       //Construct URL for first API call
-      let url = "https://api.weather.gov/points/" + String(latitude) + "," + String(longitude);
+      const url = `https://api.weather.gov/points/${latitude},${longitude}`
 
-      //Make initial API call to get local weather office and grid position
-      const req = new XMLHttpRequest();
-      req.open("GET", url, true);
-      req.send();
-      req.onload = function() {
-        const json = JSON.parse(req.responseText);
-        //Make a second API call to get the weather forecast
-        const req2 = new XMLHttpRequest();
-        req2.open("GET", json.properties.forecast, true);
-        req2.send();
-        req2.onload = function() {
-          const json2 = JSON.parse(req2.responseText);
-          console.log(json2);
-        }
+      try {
+        // Make initial API call to get local weather office and grid position
+        const response = await fetch(url);
+        const json = await response.json();
+  
+        // Make a second API call to get the weather forecast
+        const forecastUrl = json.properties.forecast;
+        const forecastResponse = await fetch(forecastUrl);
+        const forecastJson = await forecastResponse.json();
+  
+        // Update state with the weather data
+        this.setState({weather: forecastJson})
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        // Handle error, update state accordingly if needed
       }
-
-      
-    })
+    });
 
   }
 
   render() {
-    return(
-      <div>
-        <div id="page-content">
-
+    if (this.state.weather === '') {
+      return(
+        <div id='page-content' className='loading'>
+          Loading Weather Data...
         </div>
-      </div>
-    );
+      );
+    } else {
+      return(
+        <div id='page-content'>
+          
+        </div>
+      )
+    }
   }
 }
 
