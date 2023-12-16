00d1752e08217f6bcb1656908e4241f3ee9fecee
Colin Hermack <chermack@purdue.edu>
2023-12-16 14:48:06 -0500

    Changed to using the National Weather Service API

--- src/index.js
+++ src/index.js
@@ -2,34 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const API_KEY = '29a87d25c17942e883a1c75f0efa1417'  //I know how to hide an API key I just don't care because this one is free
-let url = "";
-
 class WeatherApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      latitude: 0,
-      longitude: 0
+      weather: ""
     }
   }
 
   componentDidMount() {
     //Get GPS coordinates
+    let latitude = 0;
+    let longitude = 0;
     navigator.geolocation.getCurrentPosition(position => {
-      this.setState({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      })
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+
+      //Construct URL for first API call
+      let url = "https://api.weather.gov/points/" + String(latitude) + "," + String(longitude);
+
+      //Make initial API call to get local weather office and grid position
+      const req = new XMLHttpRequest();
+      req.open("GET", url, true);
+      req.send();
+      req.onload = function() {
+        const json = JSON.parse(req.responseText);
+        //Make a second API call to get the weather forecast
+        const req2 = new XMLHttpRequest();
+        req2.open("GET", json.properties.forecast, true);
+        req2.send();
+        req2.onload = function() {
+          const json2 = JSON.parse(req2.responseText);
+          console.log(json2);
+        }
+      }
+
+      
     })
-
-    //Construct the URL for the API call
-    url = 'https://api.openweathermap.org/data/2.5/onecall?lat={'
-      + String(this.state.latitude) + '}&long={' +String(this.state.longitude) + '}&appid={' +String(API_KEY) + '}';
-
-    //Get weather data
-    fetch(url)
-      .then(response => {console.log(response)})
 
   }
 
